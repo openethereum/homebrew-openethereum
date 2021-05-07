4b32a15aa5d8a3be442cb617be8ef471c231cf12
jnr101 <jnr101@users.noreply.github.com>
2021-05-07 11:00:13 +0200

    Update openethereum.rb

--- openethereum.rb
+++ openethereum.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Openethereum < Formula
   homepage 'https://github.com/openethereum/openethereum'
-  version '3.2.4'
-  url 'https://openethereum-releases.gnosis.io/v3.2.4/macos/openethereum'
-  sha256 'e543b893f2ac2d03059935aea01db4f088f8eda35aac44857409ead18e616ec7'
+  version '3.2.5'
+  url 'https://openethereum-releases.gnosis.io/v3.2.5/macos/openethereum'
+  sha256 'f5f2d1cdfdadf4d6cbb273c733ef7ea3e1f667db769657a857ccc45727d21288'
 
   def install
     bin.install "openethereum"
